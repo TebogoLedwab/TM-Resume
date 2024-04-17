5791de1e1225d84cf6b45941c92e4641cef24ca4
Tebogo Ledwaba <Tebogo@hollard.co.za>
2024-04-17 15:32:12 +0200

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
 
-const routes: Routes = []
+const routes: Routes = [
+    { path: 'home', component: DashboardComponent},
+    { path: '/about-me', component: AboutMeComponent},
+    { path: '/contact', component: ContactMeComponent},
+    { path: '**', redirectTo: '/home', pathMatch: 'full'}
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
