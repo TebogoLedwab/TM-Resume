ce8be27627a3cddb2fb1976e84d63da5891f1832
Tebogo Ledwaba <Tebogo@hollard.co.za>
2024-06-19 15:28:53 +0200

    Added navbar solid color on scroll

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   constructor() { }
+
+  ngOnInit(): void {
+    const navEl = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 56) {
+        navEl?.classList.add('navbar-scrolled');
+      } else if (window.scrollY < 56) {
+        navEl?.classList.remove('navbar-scrolled');
+      }
+    })
+  }
 
   scrollTo(to: string) {
     switch (to) {
